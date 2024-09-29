512ec386c7dc4edfedf5ac1c0623a219aa087915
Matiss Judins <matissj1337@gmail.com>
2024-09-29 13:42:46 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <div>
+            <p className="border vorder-black rounded-md px-4 py02">
+                React Design Patterns
+            </p>
+        </div>
+    )
+}
+
+export default App
